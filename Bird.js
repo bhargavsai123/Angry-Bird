3877f23baa1d67dcffe556832c7d25729ac63d90
bhargavsai123 <61113181+bhargavsai123@users.noreply.github.com>
2020-03-13 19:57:23 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,29 @@
+class Bird {
+    constructor(x, y,) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, 50,50,options);
+      
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      pos.x = mouseX
+      pos.y = mouseY
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill(180);
+      strokeWeight(6);
+      stroke("grey");
+      rect(0, 0,50,50);
+      pop();
+    }
+  };
+  
